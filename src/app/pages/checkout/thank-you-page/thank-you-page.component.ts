659ddf5350be1db381edce19d541e8675ffca872
Juank147 <85714844+Juank147@users.noreply.github.com>
2022-01-17 07:55:52 -0500

    ZapStore
    App creada en Angular framework,
    proyecto de grado.

---
+++ src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-you-page',
+  template: `<div class="container">
+  <h1 class="title">Thankyou</h1>
+  <p class="content">Your order is no the way!
+  </p>
+  <span>
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laboriosam quia quaerat, dolore repellat commodi aperiam ducimus nulla ex? Minima laboriosam beatae ducimus sed iure in quasi reiciendis animi ad.
+  </span>
+  </div>
+  `,
+  styleUrls: ['./thank-you-page.component.scss']
+})
+export class ThankYouPageComponent  {
+
+
+}
